9418dbf2b630450cb92941a78dde1237b4b84480
User <user@Users-iMac.local>
2022-11-24 17:03:42 +0300

    Add side menu items

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,10 +1,40 @@
 <template>
    <div class="h-screen w-screen bg-lime-50 flex">
-    <div class="w-1/4 border-r h-full">
-      <Button label="Add new task" icon="PlusIcon" />
+    <div class="w-1/4 border-r h-full p-10 flex flex-col space-y-5">
+      <!-- 
       <Fab label="Add new task" icon="PlusIcon" />
       <Tag label="Add new task" icon="PlusIcon" />
-      <Card/>
+      <Card/> -->
+      <div class="font-bold text-lg">Task Master</div>
+      <Button label="Add new task" icon="PlusIcon"/>
+      <div class="w-full h-12 rounded hover:bg-yellow-100 flex items-center justify-between">
+        <div class="flex items-center space-x-2 text-sm">
+          <div class="h-8 w-8 bg-black"></div>
+          <span class="font-semibold">Plan</span>
+        </div>
+        <div class="h-5 w-5 bg-black"></div>
+      </div>
+      <div class="w-full h-12 rounded hover:bg-yellow-100 flex items-center justify-between">
+        <div class="flex items-center space-x-2 text-sm">
+          <div class="h-8 w-8 bg-black"></div>
+          <span class="font-semibold">Plan</span>
+        </div>
+        <div class="h-5 w-5 bg-black"></div>
+      </div>
+      <div class="w-full h-12 rounded hover:bg-yellow-100 flex items-center justify-between">
+        <div class="flex items-center space-x-2 text-sm">
+          <div class="h-8 w-8 bg-black"></div>
+          <span class="font-semibold">Plan</span>
+        </div>
+        <div class="h-5 w-5 bg-black"></div>
+      </div>
+      <div class="w-full h-12 rounded hover:bg-yellow-100 flex items-center justify-between">
+        <div class="flex items-center space-x-2 text-sm">
+          <div class="h-8 w-8 bg-black"></div>
+          <span class="font-semibold">Plan</span>
+        </div>
+        <div class="h-5 w-5 bg-black"></div>
+      </div>
     </div>
     <div class="w-3/4 flex flex-col">
       <div class="w-full h-24 border-b">Header</div>
