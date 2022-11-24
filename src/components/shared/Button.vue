7de36cf37848051f8f06341786fda190f32c661a
User <user@Users-iMac.local>
2022-11-24 14:35:20 +0300

    Initial commit

---
+++ src/components/shared/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="bg-lime-300 px-10 py-4 text-sm rounded font-semibold flex items-center space-x-2 shadow">
+    <component class="h-5 w-5 stroke-2" :is="Icons[props.icon]"></component>
+    <span>{{ props.label }}</span>
+  </button>
+</template>
+
+<script setup>
+import * as Icons from '@heroicons/vue/24/outline'
+
+const props =  defineProps({
+  label: [String, Number],
+  icon: [String, Number],
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="h-screen w-screen bg-lime-50 flex">
+    <div class="w-1/4 border-r h-full">
+      <Button label="Add new task" icon="PlusIcon" />
+      <Fab label="Add new task" icon="PlusIcon" />
+      <Tag label="Add new task" icon="PlusIcon" />
+      <Card/>
+    </div>
+    <div class="w-3/4 flex flex-col">
+      <div class="w-full h-24 border-b">Header</div>
+      <div class="w-full h-20 border-b">Sub-Header</div>
+      <div class="w-full h-full flex overflow-auto">
+        <div class="w-1/4 border-r flex-shrink-0 text-xs" v-for="column in [1,2,3,4,5]" :key="column">
+          Categories
+        </div>
+      </div>
+    </div>
+   </div>
+</template>
+
+<script setup>
+import Button from '../components/shared/Button.vue'
+import Fab from '../components/shared/Fab.vue'
+import Tag from '../components/shared/Tag.vue'
+import Card from '../components/shared/Card.vue'
+</script>
+
+<style>
+
+</style>
