aa79f5f5d05d5397832657ccb180a7484418da16
Kooma Benjamin <kooma.benjamin@gmail.com>
2022-11-27 13:06:34 -0800

    A user should have a the capability of navigating between (Backlog, priority chart and kanban workflow) - phase 2

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -45,57 +45,13 @@
         </div>
       </div>
 
-      <div class="w-full h-20 border-b flex items-center justify-between px-4">
-        <div class="flex space-x-4 items-center">
-          <MagnifyingGlassIcon class="w-5 h-5" />
-          <input placeholder="Search for something" class="h-full bg-lime-50" @keyup="searchCard($event)">
-        </div>
-        <div class="flex items-center space-x-2">
-          <!-- switch -->
-          <div class="flex items-center space-x-2 relative text-sm">
-            <Switch v-model="enabled" :class="enabled ? 'bg-lime-400' : 'bg-black'"
-              class="relative inline-flex h-[30px] w-[55px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-              <span class="sr-only">Use setting</span>
-              <span aria-hidden="true" :class="enabled ? 'translate-x-[25px]' : 'translate-x-0'"
-                class="pointer-events-none inline-block h-[26px] w-[26px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out" />
-            </Switch>
-            <span>Select Timeframe</span>
-            <ChevronUpIcon class="w-4"/>
-            <!-- Calendar -->
-            <Calendar v-if="enabled"/>
-          </div>
+      <KanbanWorkFlow v-if="activeTab === 'kanban-workflow'"/>
 
-          <Button label="Share" icon="ShareIcon" color="bg-transparent" size="md" drop-down="true" />
-        </div>
-      </div>
+      <PriorityChart v-if="activeTab === 'priority-chart'"/>
 
-      <div class="w-full h-full flex overflow-auto">
-        <div class="w-1/4 border-r flex-shrink-0 text-xs overflow-auto" v-for="column in columns" :key="column">
-          <!-- <Card draggable="true" @dragstart="hide(`card-${column}`)" :id="`card-${column}`" v-for="card in cards.filter((el) => el.status === column)" /> -->
-          <div 
-            class="m-2 p-5 bg-white rounded-lg h-auto border shadow" 
-            draggable="true"
-            v-for="card in cards.filter((el) => el.status === column)"
-          >
-              <div class="flex items-center justify-between">
-                <div class="flex -space-x-4">
-                  <div class="h-10 w-10 rounded-full border-2 border-slate-500 bg-black" v-for="member in card.members"></div>
-                </div>
-                <div class="w-auto px-2 bg-rose-200 text-rose-600 py-1 rounded-full font-bold captalise">{{ card.priority }}</div>
-              </div>
-              <div class="font-bold text-base my-1">A/B Testing - Round 3</div>
-              <div class="flex items-center space-x-1 my-1">
-                <div class="w-auto px-2 bg-blue-200 text-blue-600 py-1 rounded-full font-bold captalise">Prototype</div>
-                <div class="w-auto px-2 bg-green-200 text-green-600 py-1 rounded-full font-bold captalise">Research</div>
-                <div class="w-auto px-2 bg-yellow-200 text-yellow-600 py-1 rounded-full font-bold captalise">Testing</div>
-              </div>
-              <div class="flex items-center space-x-2 text-slate-500 font-semibold mt-4">
-                <CalendarIcon class="h-5 stroke-2"/>
-                <span>{{ card.date }}</span>
-              </div>
-          </div>  
-        </div>
-      </div>
+      <BackLog v-if="activeTab === 'backlog'"/>
+
+
     </div>
   </div>
 </template>
@@ -116,6 +72,9 @@ import {
   CalendarIcon,
 } from '@heroicons/vue/24/outline'
 import SideBar from '../components/shared/SideBar.vue'
+import KanbanWorkFlow from '../components/sections/KanbanWorkFlow.vue'
+import PriorityChart from '../components/sections/PriorityChart.vue'
+import BackLog from '../components/sections/BackLog.vue'
 import Button from '../components/shared/Button.vue'
 import Fab from '../components/shared/Fab.vue'
 import Tag from '../components/shared/Tag.vue'
@@ -123,34 +82,9 @@ import Card from '../components/shared/Card.vue'
 import NavItem from '../components/shared/NavItem.vue'
 import Calendar from '../components/shared/Calendar.vue'
 
-const hide = (id) => document.getElementById(id).style.visibility = 'hidden';
-
-const columns = ref(['to-do', 'refined', 'verified','doing', 'done'])
-
 const activeTab = ref('kanban-workflow')
 
 const changeTab = (tab) => activeTab.value = tab;
-
-const searchCard = (e) => {
-  // console.log(e.target.value)
-  let filteredCards = cards.value.filter((card) => card.members.includes(parseInt(e.target.value)));
-  if(e.target.value) cards.value = filteredCards;
-  console.log(cards.value)
-}
-
-const cards = ref([
-  { id: 1, status: 'doing', priority: 'high', date: '5th October 2022 - 8th October 2022', members: [1,2] },
-  { id: 2, status: 'done', priority: 'high', date: '5th October 2022 - 8th October 2022', members: [1] },
-  { id: 3, status: 'to-do', priority: 'medium', date: '5th October 2022 - 8th October 2022', members: [1,2,3] },
-  { id: 4, status: 'doing', priority: 'low', date: '5th October 2022 - 8th October 2022', members: [1,4,5] },
-  { id: 5, status: 'doing', priority: 'low', date: '5th October 2022 - 8th October 2022', members: [1,3,4,5] },
-  { id: 6, status: 'to-do', priority: 'low', date: '5th October 2022 - 8th October 2022', members: [1,3,4,5] },
-  { id: 7, status: 'to-do', priority: 'low', date: '5th October 2022 - 8th October 2022', members: [1,4,5] },
-  { id: 8, status: 'doing', priority: 'high', date: '5th October 2022 - 8th October 2022', members: [1,2,3,4,5] },
-  { id: 9, status: 'done', priority: 'high', date: '5th October 2022 - 8th October 2022', members: [1,2,3,4,5] },
-  { id: 10, status: 'verified', priority: 'medium', date: '5th October 2022 - 8th October 2022', members: [1,2,3,4,5] },
-  { id: 11, status: 'refined', priority: 'medium', date: '5th October 2022 - 8th October 2022', members: [1,2,3,4,5] },
-])
 
 const enabled = ref(false)
 
