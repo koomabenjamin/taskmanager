be35def437533c5dc764f87d80764dfd3044800a
Deogratius Ssemakula <go2ssemakula@gmail.com>
2023-02-08 08:41:55 -0500

    Create new Vue.js project

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { PlusIcon } from "@heroicons/vue/20/solid";
+</script>
+
+<template>
+  <div class="w-1/5 h-full px-5 pt-5 space-y-2">
+    <div>
+      <h3 class="text-xl font-bold">Flows list</h3>
+    </div>
+    <button
+      class="flex items-center justify-center mb-6 mx-auto h-10 w-full rounded-md bg-lime-200 text-sm"
+    >
+      <PlusIcon class="h-4 w-4" />
+      <p class="ml-2">Add New Task</p>
+    </button>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Sidebar from "../components/Sidebar.vue";
+</script>
+
+<template>
+  <div class="flex h-screen w-screen bg-neutral-300">
+    <Sidebar />
+  </div>
+</template>
