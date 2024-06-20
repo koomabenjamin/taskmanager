9ea03ad966bfbdd6a6849e5c17868856c277c485
N Jedidiah <ntaryebwa.mukama@gmail.com>
2024-06-20 21:27:03 +0300

    create AddProject.vue

--- src/components/forms/AddProject.vue
+++ src/components/forms/AddProject.vue
@@ -1,10 +1,22 @@
 <template>
-
-</template>
-<script setup>
-
-</script>
-
-<style scoped>
-
-</style>
+    <div>
+      <form @submit.prevent="submitForm">
+        <label for="name">Project Name</label>
+        <input type="text" id="name" v-model="name" required>
+        <button type="submit">Add Project</button>
+      </form>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  import axios from 'axios'
+  
+  const name = ref('')
+  
+  const submitForm = async () => {
+    await axios.post('/api/projects', { name: name.value })
+    name.value = ''
+  }
+  </script>
+  
