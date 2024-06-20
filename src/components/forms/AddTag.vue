3aa545b4ff97b6b74ed6b5ae38431a0f928c0a38
N Jedidiah <ntaryebwa.mukama@gmail.com>
2024-06-20 21:27:53 +0300

    update AddTag.vue

--- src/components/forms/AddTag.vue
+++ src/components/forms/AddTag.vue
@@ -1,10 +1,22 @@
 <template>
-
-</template>
-<script setup>
-
-</script>
-
-<style scoped>
-
-</style>
+    <div>
+      <form @submit.prevent="submitForm">
+        <label for="name">Tag Name</label>
+        <input type="text" id="name" v-model="name" required>
+        <button type="submit">Add Tag</button>
+      </form>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  import axios from 'axios'
+  
+  const name = ref('')
+  
+  const submitForm = async () => {
+    await axios.post('/api/tags', { name: name.value })
+    name.value = ''
+  }
+  </script>
+  
