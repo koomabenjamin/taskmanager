c5418ae7155497563814b08526e3bf596fcdd708
N Jedidiah <ntaryebwa.mukama@gmail.com>
2024-06-22 00:42:22 +0300

    update models- backend

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name', 'description', 'project_id', 'user_id', 'priority', 'category', 'status', 'implementation_date'
+    ];
+
+    protected $dates = [
+        'implementation_date', 'deleted_at'
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
