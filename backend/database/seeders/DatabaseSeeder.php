e51b10ac79192283b2418a39ac38620eec6d6a3d
N Jedidiah <ntaryebwa.mukama@gmail.com>
2024-06-22 00:45:58 +0300

    update DatabaseSeeder.php

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,21 +3,23 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Project;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Create 10 users, ensuring the first user has a specific email and password
+        User::factory()->janedoe()->create();
+        // Create additional users
+        User::factory()->count(9)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Create 5 projects
+        Project::factory()->count(5)->create();
+
+        // Create 5 tasks with randomly assigned user and project IDs
+        Task::factory()->count(5)->create();
     }
 }
